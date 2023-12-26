b5abf33ff57609b5953fce1154a62a8740a0d8e6
takeape <you@example.com>
2023-12-26 18:17:28 +0300

    Vers 1.0

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<style>
+body { background-color: brown }
+</style>
+</head>
+<body >
+<center>
+<font color=white>
+<h1>Hello From DevOps WebPage! Version-2</h1>
+</font>
+</center>
+
+<?php
+
+echo '<p style="color orange; text-align: center">
+	Hello From AWS Elastic Beanstalk WebServer!
+	</p>';
+
+echo '<br>Local IP: '.$_SERVER['SERVER_ADDR'];
+?>
+
+</body>
+</html>
+
